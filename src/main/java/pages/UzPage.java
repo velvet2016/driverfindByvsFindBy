b2b83cf9c9ba61ed325f614ddd8f4fb8dbf12488
velvet2016 <astudzinsky@gmail.com>
2019-03-01 17:48:50 +0200

    comment added

--- src/main/java/pages/UzPage.java
+++ src/main/java/pages/UzPage.java
@@ -24,7 +24,7 @@ public class UzPage {
     }
 
     public static final String KYIV = "Kyiv";
-    public static final String XPATH_CITY_FROM_DROP_DOWN_KYIV = "//ul[@id='ui-id-1']/li[text()='"+ KYIV +"']";
+    public static final String XPATH_CITY_FROM_DROP_DOWN_KYIV = "//ul[@id='ui-id-1']/li[text()='" + KYIV + "']";
     public static final String ODESA = "Odesa";
     public static final String XPATH_CITY_TO_DROP_DOWN_ODESA = "//ul[@id='ui-id-2']/li[text()='" + ODESA + "']";
 
@@ -35,36 +35,49 @@ public class UzPage {
     @FindBy(name = "to-title")
     private WebElement inputTo;
 
+    @FindBy(xpath = XPATH_CITY_FROM_DROP_DOWN_KYIV)
+    private WebElement cityFromDropDownKyiv;
 
-
-    @FindBy(name = XPATH_CITY_TO_DROP_DOWN_ODESA)
+    @FindBy(xpath = XPATH_CITY_TO_DROP_DOWN_ODESA)
     private WebElement cityToDropDownOdesa;
-
 
 
     public Logger logger = Logger.getLogger(this.getClass());
 
-    @FindBy(xpath = XPATH_CITY_FROM_DROP_DOWN_KYIV)
-    private WebElement cityFromDropDownKyiv;
 
-    public void setCityFromKyiv(){
+    public void setCityFromKyiv() {
         inputFrom.click();
         inputFrom.sendKeys(KYIV);
-        //WebElement cityFromDropDownKyiv = driver.findElement(By.xpath(XPATH_CITY_FROM_DROP_DOWN_KYIV));
+        WebElement cityFromDropDownKyiv = driver.findElement(By.xpath(XPATH_CITY_FROM_DROP_DOWN_KYIV));
         waitForVisibility(cityFromDropDownKyiv);
+        cityFromDropDownKyiv.click();
 
     }
-    public void setCityToOdesa(){
+
+    public void setCityToOdesa() {
         inputTo.click();
         inputTo.sendKeys(ODESA);
-        driver.manage().timeouts().implicitlyWait(0, TimeUnit.MILLISECONDS);
         waitForVisibility(cityToDropDownOdesa);
+        cityToDropDownOdesa.click();
     }
 
-    protected void waitForVisibility(WebElement element) throws Error{
+
+
+
+    /*
+    If input param element is initialized using annotation @FindBy wait works correctly
+    If input param element is initialized by calling driver.findElement(...) method than
+        wait is not applied and NoSuchElement exception is being thrown.
+    */
+    protected void waitForVisibility(WebElement element) throws Error {
         logger.debug("waiting till element " + element.toString() + " loaded");
-        new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOf(element));
-        logger.debug("waiting till element " + element.toString() + " loaded...Done" );
+        /*incorrect using ExpectedConditions.visibilityOf(element). Method visibilityOf(element)
+        supposed to be used when it is known that element is present in DOM, just for checking visibility.
+        Used incorrectly just to show different behavior between @FindBy and direct finding element
+        by driver.findElement()
+        */
+        new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOf(element));
+        logger.debug("waiting till element " + element.toString() + " loaded...Done");
     }
 
 }
